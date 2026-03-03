706aa6da15cde3db100f0f97e252387ac2b9b727
LUIZ EDUARDO SANTOS DE ALBUQUERQUE <luiz.esalbuquerque@senacsp.edu.br>
2026-03-03 09:53:47 -0300

    aTT

---
+++ 04-atividade/script.js
@@ -0,0 +1,27 @@
+// Exercicio 1
+
+let opcao = "morango"
+
+ switch(opcao){
+     case "chocolate":
+     console.log("Escolhido o de chocolate!");
+     break;
+     case "morango":
+        console.log("Um bolo de morango chegando!");
+        break;
+        case "leite ninho":
+            console.log("Escolhido o de leite ninho");
+            break;
+            default:
+                console.log("Não temos esse produto!")
+
+ }
+
+    //Exercicio 2
+
+    let b = 1;
+
+    while (b <= 5){
+        console.log(b);
+        b = b + 1;
+    }
